34f8f294b0f4fdcf6bbd6cb4c4f5e62458f087c5
Tomasz Kazimierczuk <Tomasz.Kazimierczuk@fuw.edu.pl>
2018-08-23 18:37:01 +0200

    Data is additionally saved in .txt format if possible.

--- src/measurement_file.py
+++ src/measurement_file.py
@@ -3,6 +3,7 @@
 import h5py
 import devices
 from numpy import ndarray,array
+import numpy as np
 
 class MeasurementFile:
     def __init__(self, filename, x_data=None, y_data=None, z_data=None):
@@ -14,6 +15,7 @@ class MeasurementFile:
             """
         self.n = 0
         self.file = h5py.File(filename, 'w')
+        self.filename = filename
         
         self.main_data = {}
         for axis,source in (('x',x_data), ('y',y_data), ('z',z_data)):
@@ -31,10 +33,44 @@ class MeasurementFile:
     def __del__(self):
         self.close()
     
+    def save_txt(self):
+        """ Try to write ASCII file with the main data """
+        print("Attempting to export txt file")
+        if 'z' not in self.file:
+            print("No Z data in the file")
+            return
+        z = array(self.file['z'])
+        if len(z.shape) != 2:
+            print("Z data is not 2D array")
+            return
+        
+        offset=0
+        if 'x' in self.file:
+            x = array(self.file['x'])
+            if len(x.shape)!=1 or len(x) != z.shape[1]:
+                print("Mismatch between the size of x (%d) and z (%d)" %(len(x), z.shape[1]))
+                return
+            z = np.concatenate((x[np.newaxis,:],z),axis=0)
+            offset=1
+        
+        if 'y' in self.file:
+            y = array(self.file['y'])
+            if len(y.shape)!=1 or len(y) != z.shape[0]-offset:
+                print("Mismatch between the size of y (%d) and z (%d)" %(len(x), z.shape[0]-offset))
+                return
+            if offset:
+                y = np.r_[0,y]
+            z = np.r_[y,z]
+        
+        np.savetxt(self.fname+'.txt',z)
+        
+    
     def close(self):
+        self.save_txt()
         self.file.close()
         
     def save_snapshot(self, **user_data):
+        """ We save the state of all active devices """
         self.n += 1
         for key,func in self.main_data.items():
             self._save_data(self.file, key, func())
@@ -58,14 +94,21 @@ class MeasurementFile:
         for key,value in data.items():
             self._save_data(group, key, value)
                 
+                
     def _save_data(self, group, name, value):
+        if type(value) == list:
+            try:
+                value=array(value)
+            except:
+                pass
+        #now we start actual saving:
         if type(value) == dict:
             self._save_subgroup(group, name, value)
         elif type(value) in (int, float, bool, ndarray):
             value = array(value)
             newshape = (self.n,) + value.shape
             if name not in group:
-                dset = group.create_dataset(name, shape=newshape, maxshape=(None,), dtype=value.dtype)
+                dset = group.create_dataset(name, shape=newshape, maxshape=(None,)+value.shape, dtype=value.dtype)
             else:
                 dset = group[name]
                 dset.resize( newshape )
